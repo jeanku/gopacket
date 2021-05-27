1e2e98ff3722c4d3ac33b534ca1471822550f26b
JeanKu <490573621@qq.com>
2021-05-27 17:40:46 +0800

    dd

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jeanku/gosql
+
+go 1.14
---
+++ model.go
@@ -0,0 +1,8 @@
+package gosql
+
+import "fmt"
+
+// say Hi to someone
+func SayHi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
